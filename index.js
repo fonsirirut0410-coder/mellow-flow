2956c1390296bd567cd2ae09fcaa60f5eed94f22
fonsirirut0410-coder <fonsirirut0410@gmail.com>
2026-05-28 14:00:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.get("/", (req, res) => {
   res.send("Mellow Flow is running 🌊");
 });
 
-app.get("/", (req, res) => {
-  res.send("Mellow Flow is running 🌊");
-});
-
 app.get("/webhook", (req, res) => {
   res.send("Webhook route exists");
+});
+
+app.post("/webhook", line.middleware(config), async (req, res) => {
+  res.sendStatus(200);
 });
 
 app.post("/webhook", line.middleware(config), async (req, res) => {
